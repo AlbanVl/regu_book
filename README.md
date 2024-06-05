0733ef506b18d96096167d8cc7ae9615c1fb31e7
Alban VAN LAETHEM <a.vanlaethem@helmo.be>
2024-06-05 09:03:00 +0200

    Add the python file corrector `correct_latex.py`  for the Latex generated files

--- README.md
+++ README.md
@@ -2,14 +2,24 @@
 
 1. Follow the MyST tool given on the [MyST site](https://mystmd.org/guide/installing)
 1. Install InkSkape (problème avec les conversions des svg => doit installer à partir du site de IknSkape et non la version directe d'Ubuntu):
-    1. sudo add-apt-repository ppa:inkscape.dev/stable
-    1. sudo apt update
-    1. sudo apt-get install inkscape=1:1.3.2+202311252150+091e20ef0f~ubuntu22.04.1
+    1. `sudo add-apt-repository ppa:inkscape.dev/stable`
+    1. `sudo apt update`
+    1. `sudo apt-get install inkscape=1:1.3.2+202311252150+091e20ef0f~ubuntu22.04.1`
 
+## Generate the pdf version
+
+1. Install the **full** version of Latex : https://www.tug.org/texlive/quickinstall.html ;
+1. Generate the pdf via Latex files :
+    1. Open a terminal in the main folder ;
+    1. `myst build --tex nameOfTheFile.tex` ;
+    1. Copy the `correct_latex.py` into the folder containing the Latex files : `cp correct_latex.py _build/exports/nameOfTheFile_tex/` ;
+    1. Go into the folder containing the Latex files : `cd _build/exports/nameOfTheFile_tex/` ;
+    1. Run the python file : `python3 correct_latex.py` ;
+    1. Generate the pdf via Latex : `pdflatex nameOfTheFile.tex`.
 
 # TODO List
 
-- [ ] Gérer les [Subfigures](https://mystmd.org/guide/figures#subfigures) au niveau du pdf.
+- [x] Gérer les [Subfigures](https://mystmd.org/guide/figures#subfigures) au niveau du pdf.
     Exemple : Seance5:
 
     Les figure imbriquées sont imbriquées sous forme de figure et non de sous-figures.
@@ -19,36 +29,44 @@
     ```
     \begin{figure}[!htbp]
     \centering
-    \begin{subfigure}
+    \begin{subfigure}{\textwidth}
     \centering
     \includegraphics[width=0.7\linewidth]{files/GeneralSimpleFeedbac-5b4b02a1572b03b8e10ce7e4256c9621.png}
     \caption[]{}
     \label{my-figure-a}
     \end{subfigure}
     
-    \begin{subfigure}
+    \begin{subfigure}{\textwidth}
     \centering
     \includegraphics[width=0.7\linewidth]{files/GeneralSimpleFeedbac-169444c7871d084a515f37dc9aec82e6.png}
     \caption[]{}
     \label{my-figure-b}
     \end{subfigure}
     
-    \begin{subfigure}
+    \begin{subfigure}{\textwidth}
     \centering
     \includegraphics[width=0.7\linewidth]{files/GeneralSimpleFeedbac-cb7b1f89a0b8757e5e6891793ef0575f.png}
     \caption[]{}
     \label{my-figure-c}
     \end{subfigure}
     
-    \begin{subfigure}
+    \begin{subfigure}{\textwidth}
     \centering
     \includegraphics[width=0.7\linewidth]{files/GeneralSimpleFeedbac-e6baa06324cd9b3fa55d592e0342e0b7.png}
     \caption[]{}
     \label{my-figure-d}
     \end{subfigure}
     \caption[]{Résolution du système pour $D_{in}$}
-    \label{my-figure-d}
+    \label{my-figure}
     \end{figure}
     ```
 
 - [ ] L'intégration d'une image dans un avertissement (admonition) ne fonctionne pas (Exemple : Seance5, même endroit que le précédent)
+
+- [ ] Problème de renvoi vers la figure 4 de la séance 3, section : Stabilité d’un système dans le domaine fréquentiel
+
+- [ ] Mettre une image pour marquer la présence d'une vidéo avec le lien hypertexte permmettant d'y aller.
+
+- [ ] Gérer le problème d'affichage des noms des Parts trop longs.
+
+- [ ] Modifier le look des citations pour ressembler à ce qu'on obtient sur le site (cf. Séance 3 section 4).
---
+++ correct_latex.py
@@ -0,0 +1,101 @@
+import os
+import fnmatch
+import re
+
+"""
+This script corrects the admonitions in a given .tex file.
+It corrects the admonitions by replacing the framed environment with the appropriated block from the awesomebox package.
+
+Usage:
+    1. Place this script in the same directory as your .tex files
+    2. Run the script
+    3. The script will correct all .tex files in the directory
+"""
+
+def correct_subfigures(text: str) -> str:
+    """
+    Corrects the subfigures in the given text.
+    Replaces the figure environment with the subfigure environment.
+
+    Args:
+        text (str): The text containing the subfigures
+
+    Returns:
+        str: The text with the corrected subfigures
+    """
+
+    # Define the pattern for a main figure environment
+    main_figure_pattern = r"(\\begin{figure}\[!htbp\]\s*\\centering\s*\\begin{figure}\[!htbp\].*?\\end{figure}\s*\\caption\[\]{.*?}\s*\\label{.*?}\s*\\end{figure})"
+
+    # Define the pattern for a nested figure environment
+    nested_figure_pattern = r"(\\begin{subfigure}.*?\\end{figure})"
+
+    # Find all main figure environments
+    main_figures = re.findall(main_figure_pattern, text, re.DOTALL)
+
+    # For each main figure environment, replace \begin{figure} and \end{figure} with \begin{subfigure} and \end{subfigure}
+    modified_main_figures = []
+    for main_figure in main_figures:
+        # Replace \begin{figure} with \begin{subfigure}
+        main_figure = re.sub(r"\n\\begin{figure}\[!htbp\]", r"\n\\begin{subfigure}", main_figure)
+        # Find all nested figure environments
+        nested_figures = re.findall(nested_figure_pattern, main_figure, re.DOTALL)
+        # Replace \begin{figure} and \end{figure} with \begin{subfigure} and \end{subfigure} in each nested figure environment
+        modified_nested_figures = [re.sub(r"\\begin{subfigure}", r"\\begin{subfigure}{\\textwidth}", re.sub(r"\\end{figure}", r"\\end{subfigure}", nested_figure)) for nested_figure in nested_figures]
+        # Replace each nested figure environment in the main figure environment with the modified nested figure environment
+        for nested_figure, modified_nested_figure in zip(nested_figures, modified_nested_figures):
+            main_figure = main_figure.replace(nested_figure, modified_nested_figure)
+        # Correct the label of the main figure environment
+        main_figure = re.sub(r"\\label{(.*?)-(.*?)}\s*\\end{figure}", r"\\label{\1}\n\\end{figure}", main_figure)
+        # Append the modified main figure environment to the list of modified main figures
+        modified_main_figures.append(main_figure)
+
+    # Replace each main figure environment in the text with the modified main figure environment
+    for main_figure, modified_main_figure in zip(main_figures, modified_main_figures):
+        text = text.replace(main_figure, modified_main_figure)
+    
+    return text
+
+# Function to correct the admonitions in the given text
+def correct_admonitions(text: str) -> str:
+    """
+    Corrects the admonitions in the given text.
+    Replaces the framed environment with the tipblock or noteblock environment.
+
+    Args:
+        text (str): The text containing the admonitions
+
+    Returns:
+        str: The text with the corrected admonitions
+    """
+    
+    text = re.sub(r'\\begin\{framed\}\s*\\textbf\{Hint\}\\\\(.*?)\\end\{framed\}', r'\\begin{tipblock}\1\\end{tipblock}', text, flags=re.DOTALL)
+    text = re.sub(r'\\begin\{framed\}\s*\\textbf\{Note\}\\\\(.*?)\\end\{framed\}', r'\\begin{noteblock}\1\\end{noteblock}', text, flags=re.DOTALL)
+    text = re.sub(r'\\begin\{framed\}\s*\\textbf\{Attention\}\\\\(.*?)\\end\{framed\}', r'\\begin{warningblock}\1\\end{warningblock}', text, flags=re.DOTALL)
+    text = re.sub(r'\\begin\{framed\}\s*\\textbf\{Warning\}\\\\(.*?)\\end\{framed\}', r'\\begin{warningblock}\1\\end{warningblock}', text, flags=re.DOTALL)
+    text = re.sub(r'\\begin\{framed\}\s*\\textbf\{Important\}\\\\(.*?)\\end\{framed\}', r'\\begin{importantblock}\1\\end{importantblock}', text, flags=re.DOTALL)
+    text = re.sub(r'\\begin\{framed\}\s*\\textbf\{Caution\}\\\\(.*?)\\end\{framed\}', r'\\begin{cautionblock}\1\\end{cautionblock}', text, flags=re.DOTALL)
+    text = re.sub(r'\\begin\{framed\}\s*\\textbf\{Question\}\\\\(.*?)\\end\{framed\}', r'\\begin{awesomeblock}[teal]{2pt}{\\faQuestionCircle}{teal}\1\\end{awesomeblock}', text, flags=re.DOTALL)
+    text = re.sub(r'\\begin\{framed\}\s*\\textbf\{Exercice\}\\\\(.*?)\\end\{framed\}', r'\\begin{awesomeblock}[violet]{2pt}{\\faEdit}{violet}\1\\end{awesomeblock}', text, flags=re.DOTALL)
+    return text
+
+def correct_tex_file(file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    # Correct the admonitions in the given file
+    content = correct_admonitions(content)
+    # Correct the subfigures in the given file
+    content = correct_subfigures(content)
+
+    with open(file_path, 'w') as file:
+        file.write(content)
+
+def correct_all_tex_files():
+    for file in os.listdir('.'):
+        if fnmatch.fnmatch(file, '*.tex'):
+            correct_tex_file(file)
+
+# Call the function with the path to your .tex file
+if __name__ == '__main__':
+    correct_all_tex_files()
